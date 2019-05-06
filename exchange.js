f827390ec7d15ab368a5152b228d53924a4ac285
KrzysztofSobota <ksobota1981@gmail.com>
2019-05-06 21:34:41 +0200

    Major changes
    HTML:
    - insert logo,
    - split page into article/sections/footer,
    - change table from div with class into real <table>,
    - add canvas as a future chart
    CSS:
    - changes connected with HTML
    JS:
    - change some Math.random(),
    - change var to let,
    - change innerHTML to textContent,
    - changes in addeventlistener parts,

--- exchange.js
+++ exchange.js
@@ -5,37 +5,38 @@ function value() {
 let currency = [];
     if (currency.length < 8) {
     /* General Min/Max limits for buy values */
-        var USD_buy = (Math.floor(10000 * ((Math.random() * 1.8) + 2.7)))/10000;
-        var EUR_buy = (Math.floor(10000 * ((Math.random() * 1.6) + 3.2)))/10000;
-        var GBP_buy = (Math.floor(10000 * ((Math.random() * 2) + 4.5)))/10000;
-        var CHF_buy = (Math.floor(10000 * ((Math.random() * 2.5) + 2)))/10000;
-    /* a,b,c,d sell/buy parameters are made for independently calculating sell value for every currencies */
-        var a = (Math.random() * 0.01) + 1.015;
-        var b = (Math.random() * 0.01) + 1.015;    
-        var c = (Math.random() * 0.01) + 1.015;    
-        var d = (Math.random() * 0.01) + 1.015;    
-        var USD_sell = (Math.floor(10000 * a * USD_buy))/10000;
-        var EUR_sell = (Math.floor(10000 * b * EUR_buy))/10000;
-        var GBP_sell = (Math.floor(10000 * c * GBP_buy))/10000;
-        var CHF_sell = (Math.floor(10000 * d * CHF_buy))/10000;
+       let USD_buy = ((Math.random() * 1.8) + 2.7).toFixed(3);
+       let EUR_buy = ((Math.random() * 1.6) + 3.2).toFixed(3);
+       let GBP_buy = ((Math.random() * 2) + 4.5).toFixed(3);
+       let CHF_buy = ((Math.random() * 2.5) + 2).toFixed(3);
+      
+/* a,b,c,d sell/buy parameters are made for independently calculating sell value for every currencies */
+        let a = (Math.random() * 0.01) + 1.015;
+        let b = (Math.random() * 0.01) + 1.015;    
+        let c = (Math.random() * 0.01) + 1.015;    
+        let d = (Math.random() * 0.01) + 1.015;    
+        let USD_sell = (Math.floor(10000 * a * USD_buy))/10000;
+        let EUR_sell = (Math.floor(10000 * b * EUR_buy))/10000;
+        let GBP_sell = (Math.floor(10000 * c * GBP_buy))/10000;
+        let CHF_sell = (Math.floor(10000 * d * CHF_buy))/10000;
     } 
     else {
     /* +/-5% fluctuation of exchange rate (preventing deep up/down moving) */
-      var USD_buy = (Math.floor(10000 * ((Math.random() * 0.1) + 0.95) * currency[0]))/10000;
-      var EUR_buy = (Math.floor(10000 * ((Math.random() * 0.1) + 0.95) * currency[2]))/10000;
-      var GBP_buy = (Math.floor(10000 * ((Math.random() * 0.1) + 0.95) * currency[4]))/10000;
-      var CHF_buy = (Math.floor(10000 * ((Math.random() * 0.1) + 0.95) * currency[6]))/10000;
-      var USD_sell = (Math.floor(10000 * ((Math.random() * 0.1) + 0.95) * currency[1]))/10000;
-      var EUR_sell = (Math.floor(10000 * ((Math.random() * 0.1) + 0.95) * currency[3]))/10000;
-      var GBP_sell = (Math.floor(10000 * ((Math.random() * 0.1) + 0.95) * currency[5]))/10000;
-      var CHF_sell = (Math.floor(10000 * ((Math.random() * 0.1) + 0.95) * currency[7]))/10000;
+      let USD_buy = (Math.floor(10000 * ((Math.random() * 0.1) + 0.95) * currency[0]))/10000;
+      let EUR_buy = (Math.floor(10000 * ((Math.random() * 0.1) + 0.95) * currency[2]))/10000;
+      let GBP_buy = (Math.floor(10000 * ((Math.random() * 0.1) + 0.95) * currency[4]))/10000;
+      let CHF_buy = (Math.floor(10000 * ((Math.random() * 0.1) + 0.95) * currency[6]))/10000;
+      let USD_sell = (Math.floor(10000 * ((Math.random() * 0.1) + 0.95) * currency[1]))/10000;
+      let EUR_sell = (Math.floor(10000 * ((Math.random() * 0.1) + 0.95) * currency[3]))/10000;
+      let GBP_sell = (Math.floor(10000 * ((Math.random() * 0.1) + 0.95) * currency[5]))/10000;
+      let CHF_sell = (Math.floor(10000 * ((Math.random() * 0.1) + 0.95) * currency[7]))/10000;
     }
 currency.push(USD_buy, USD_sell, EUR_buy, EUR_sell, GBP_buy, GBP_sell, CHF_buy, CHF_sell);
     
 /* Function changes number of digits after the dot in all currencies to 4 (3.42 -> 3.4200, 2.864 -> 2.8640, etc.) */
   function numberLength(x,y) {
     for (let i=x; i<=y; i++) {
-       var txt = currency[i].toString();  
+       let txt = currency[i].toString();  
           if (txt.length == 1) {currency[i] = currency[i] + ".0000";}
           else if (txt.length == 3) {currency[i] = currency[i] + "000";}
           else if (txt.length == 4) {currency[i] = currency[i] + "00";}
@@ -45,22 +46,22 @@ currency.push(USD_buy, USD_sell, EUR_buy, EUR_sell, GBP_buy, GBP_sell, CHF_buy, 
     return currency;
   }
 
-numberLength(0,7)
-var currencySymbols = ["USD_buy", "USD_sell", "EUR_buy", "EUR_sell", "GBP_buy", "GBP_sell", "CHF_buy", "CHF_sell"];
+numberLength(0,7);
+let currencySymbols = ["USD_buy", "USD_sell", "EUR_buy", "EUR_sell", "GBP_buy", "GBP_sell", "CHF_buy", "CHF_sell"];
     
-  for (let i = 0; i <= 7; i++) { document.getElementById(currencySymbols[i]).innerHTML=currency[i];
+  for (let i = 0; i <= 7; i++) { document.querySelector(currencySymbols[i]).textContent=currency[i];
   }
 
   if (currency.length >= 16) {
-     var Signs = ["USD_buy_Sign", "USD_sell_Sign", "EUR_buy_Sign", "EUR_sell_Sign", "GBP_buy_Sign", "GBP_sell_Sign", "CHF_buy_Sign", "CHF_sell_Sign"];
+     let Signs = ["USD_buy_Sign", "USD_sell_Sign", "EUR_buy_Sign", "EUR_sell_Sign", "GBP_buy_Sign", "GBP_sell_Sign", "CHF_buy_Sign", "CHF_sell_Sign"];
      numberLength(8,15)
       for (i = 0; i <= 7; i++) {
-         if (currency[i+8] < currency[i]) { document.getElementById(currencySymbols[i]).innerHTML=currency[i+8]; document.getElementById(Signs[i]).innerHTML="\u2193"; //low
+         if (currency[i+8] < currency[i]) { document.querySelector(currencySymbols[i]).textContent=currency[i+8]; document.querySelector(Signs[i]).textContent="\u2193"; //low
          } 
           else if (currency[i+8] == currency[i]) 
-         { document.getElementById(currencySymbols[i]).innerHTML=currency[i+8]; document.getElementById(Signs[i]).innerHTML="\u2500"; //no change
+         { document.querySelector(currencySymbols[i]).textContent=currency[i+8]; document.querySelector(Signs[i]).textContent="\u2500"; //no change
          } 
-          else { document.getElementById(currencySymbols[i]).innerHTML=currency[i+8]; document.getElementById(Signs[i]).innerHTML="\u2191"; //high
+          else { document.querySelector(currencySymbols[i]).textContent=currency[i+8]; document.querySelector(Signs[i]).textContent="\u2191"; //high
          } 
       }
       // Remove "absolete" part of 16-index table
