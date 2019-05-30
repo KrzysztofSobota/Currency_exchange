8adf76c22f7d1bbf29ab54056589e51e074befa1
KrzysztofSobota <ksobota1981@gmail.com>
2019-05-30 23:56:46 +0200

    HTML, CSS, JS changes
    CSS changes:
    - site colors,
    - margin in h1-h3,
    - table dimensions,
    HTML:
    - more elements add inside chart <div>,
    - table is without 'change' class,
    JS:
    - code is easier to read (less code lines),
    - 2 functions and 'for' loop change

--- exchange.js
+++ exchange.js
@@ -3,90 +3,70 @@
 /*window.addEventListener('DOMContentLoaded', value(), false);*/
 
 function value() {
-  let currency = [];
-    
-	/* Min/Max range for currency buy values */
-	let USD_buy = (Math.random() * 1.8) + 2.7;
-	let EUR_buy = (Math.random() * 1.6) + 3.2;
-	let GBP_buy = (Math.random() * 2) + 4.5;
-	let CHF_buy = (Math.random() * 2.5) + 2;
+	
+		/* Min/Max range for currency buy values */
+		let USD_buy = (Math.random() * 1.8) + 2.7;
+		let EUR_buy = (Math.random() * 1.6) + 3.2;
+		let GBP_buy = (Math.random() * 2) + 4.5;
+		let CHF_buy = (Math.random() * 2.5) + 2;
 
-	const buyArray = [USD_buy, EUR_buy, GBP_buy, CHF_buy];
-
-	function BuySell() {
-		let a_parameter = [];
-
-			for (let i = 0; i <= 3; i++) {
-				/* 'a' is the sell/buy parameter - loop is made for
-				independently calculating value for every currency */
-				let a = (Math.random() * 0.01) + 1.015;
-				a_parameter.push(a);
-			}
-
-		let USD_sell = a_parameter[0] * buyArray[0];
-		let EUR_sell = a_parameter[1] * buyArray[1];
-		let GBP_sell = a_parameter[2] * buyArray[2];
-		let CHF_sell = a_parameter[3] * buyArray[3];
+		let buy = [USD_buy, EUR_buy, GBP_buy, CHF_buy];
 		
-		const values = [USD_sell, EUR_sell, GBP_sell, CHF_sell];
+	/* Making 8-elements (4 buy, 4 sell) value array */
+		function SellBuy1() {
+			let sell = [];
+				for (let i = 0; i <= 3; i++) {
+			/* 'a' parameter change 4 buy to 4 sell values */
+					let a = (Math.random() * 0.01) + 1.015; // 1,5% to 2,5% more
+					sell.push(a * buy[i]);
+				}
 
-		return values;
+			return [...buy, ...sell];
+		}
+
+		let valuesArray1 = SellBuy1();
+
+//	(Math.floor(10000 * a * USD_buy))/10000
+		
+	function SellBuy2() {
+		let tempArray = [];
+			for (let i = 0; i <= 7; i++) {
+	/* 'b' is +/- 5% fluctuation of exchange rate (it prevents deep up/down value change) - 8 different numbers for sell and buy values */
+				let b = (Math.random() * 0.1) + 0.95;
+				tempArray.push(b * valuesArray1[i]);        
+			}
+		
+		return tempArray;
 	}
 
-	let sellArray = BuySell(buyArray);
-	currency = [...buyArray, ...sellArray];
-
+	let valuesArray2 = SellBuy2();
+//alert(`${valuesArray1.length} , ${valuesArray2.length}`);
+	let currency = [...valuesArray1, ...valuesArray2];
+	
 	/* Function changes number of digits after the dot in 
 	all currency values to 4 (3.42 -> 3.4200, 2.86474 -> 2.8647, etc.) */
 	let tableValues = currency.map(x => x.toFixed(4));
 	
-//	(Math.floor(10000 * a * USD_buy))/10000
-	
-      function secondArray() {
-				let b_parameter = [];
-
-					for (let i = 0; i <= 7; i++) {
-					/* 'b' is +/-5% fluctuation of exchange rate (preventing deep up/down moving) 
-					- loop is made for independently calculating sell values for every currency */
-						let b = (Math.random() * 0.1) + 0.95;
-						b_parameter.push(b);        
-					}
-
-				let USD_buy = b_parameter[0] * currency[0];
-				let USD_sell = b_parameter[1] * currency[1];
-				let EUR_buy = b_parameter[2] * currency[2];
-				let EUR_sell = b_parameter[3] * currency[3];
-				let GBP_buy = b_parameter[4] * currency[4];
-				let GBP_sell = b_parameter[5] * currency[5];
-				let CHF_buy = b_parameter[6] * currency[6];
-				let CHF_sell = b_parameter[7] * currency[7];
-				
-				return b_parameter;
-			}
-
-			let num = secondArray(currency);
-    
-   let currencySymbols = ["USD_buy", "USD_sell", "EUR_buy", "EUR_sell", "GBP_buy", "GBP_sell", "CHF_buy", "CHF_sell"];
+  let currencySymbols = ["USD_buy", "EUR_buy", "GBP_buy", "CHF_buy", "USD_sell", "EUR_sell", "GBP_sell", "CHF_sell"];
     
   for (let i = 0; i <= 7; i++) {
     document.getElementById(currencySymbols[i]).textContent = tableValues[i];
   }
 
   if (currency.length >= 16) {
-     let Signs = ["USD_buy_Sign", "USD_sell_Sign", "EUR_buy_Sign", "EUR_sell_Sign", "GBP_buy_Sign", "GBP_sell_Sign", "CHF_buy_Sign", "CHF_sell_Sign"];
+     let Signs = ["USD_buy_Sign", "EUR_buy_Sign", "GBP_buy_Sign", "CHF_buy_Sign", "USD_sell_Sign", "EUR_sell_Sign", "GBP_sell_Sign", "CHF_sell_Sign"];
      
-         
-      for (let i = 0; i <= 7; i++) {
-         if (currency[i+8] < currency[i]) {
-           document.getElementById(currencySymbols[i]).textContent = currency[i+8]; document.getElementById(Signs[i]).textContent="\u2193"; //low
-         } 
-         else if (currency[i+8] == currency[i]) {
-           document.getElementById(currencySymbols[i]).textContent = currency[i+8]; document.getElementById(Signs[i]).textContent="\u2500"; //no change
-         } 
-         else {
-           document.getElementById(currencySymbols[i]).textContent = currency[i+8]; document.getElementById(Signs[i]).textContent="\u2191"; //high
-         } 
-      }
+		for (let i = 0; i <= 7; i++) {
+			 if (currency[i+8] < currency[i]) {
+				 document.getElementById(currencySymbols[i]).textContent = `${tableValues[i+8]} ${'\u2193'}`; //low
+			 } 
+			 else if (currency[i+8] == currency[i]) {
+				 document.getElementById(currencySymbols[i]).textContent = `${tableValues[i+8]} ${'\u2500'}`; //no change
+			 } 
+			 else {
+				 document.getElementById(currencySymbols[i]).textContent = `${tableValues[i+8]} ${'\u2191'}`; //high
+			 } 
+		}
       
     // Remove "absolete" part of 16-index table
       currency.splice(0,8);
