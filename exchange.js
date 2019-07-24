ee4431b4da6d471f194e981ba7dde727aa2b08ae
KrzysztofSobota <ksobota1981@gmail.com>
2019-07-24 23:05:58 +0200

    objects in JS, HTML tags fixing
    JS:
    Object method instead for-loop
    1) buy/sell inside my objects are always numbers when moving between any functions,
    2) no need to transform string <-> numbers in code
    3) dividing array into 2 parts (buy/sell numbers) give a better control (sell values are only needed at the beginning, because after first time interval they are made from buy values only)

--- exchange.js
+++ exchange.js
@@ -17,19 +17,17 @@ function value() {
 	}
 
 	// a = (Math.random() * 0.01) + 1.015 parameter changes 4 buy to 4 sell values and makes sell values always 1,5%-2,5% more than buy values
-	let sell = buy.map(i => i * ((Math.random() * 0.01) + 1.015)); // [USD_sell, EUR_sell, GBP_sell, CHF_sell]
+	let values = {
+		buy: buy,
+		sell: buy.map(i => i * ((Math.random() * 0.01) + 1.015)) // [USD_sell, EUR_sell, GBP_sell, CHF_sell]
+	};
 
-	let valuesArray1 = [];
-	for (let index = 0; index < buy.length; index++) {
-		// buy, sell, buy, sell,... etc.
-		valuesArray1.push(buy[index], sell[index]);
+	for (let i = 0; i < values.length / 2; i++) {
+		cellText[i].textContent = `${tableValues(values.buy)[i]}`;
+		cellText[i + 1].textContent = `${tableValues(values.sell)[i]}`;
 	}
 
-	for (let i = 0; i < valuesArray1.length; i++) {
-		cellText[i].textContent = `${tableValues(valuesArray1)[i]}`;
-	}
-
-	return valuesArray1;
+	return values;
 }
 
 /* Function changes number of digits after the dot in all currency values to 4 (3.42 -> 3.4200, 2.86474 -> 2.8647, etc.) */
@@ -41,87 +39,67 @@ let cellText = document.querySelectorAll('.value');
 /*** Making another 8-elements value array from previous array (making 16-elements full array) ***/
 
 function fillTable() {
-	let Array1 = value();
-
-	console.log(Array1);
-
-	// let pos = Array.from(cellText.keys());
+	let buy_part1 = value().buy;
+	let sell_part1 = value().sell;
 
 	function dataTable() {
 		// b = (Math.random() * 0.1) + 0.95 is +/- 5% fluctuation of exchange rate (it prevents huge value change between steps)
-		let valuesArray2 = Array1.map(i => i * ((Math.random() * 0.1) + 0.95));
+		let buy_part2 = buy_part1.map(i => i * ((Math.random() * 0.1) + 0.95));
 
-		for (let j = 0; j < valuesArray2.length;) {
-			if (valuesArray2[`${j}`] > valuesArray2[`${j+1}`]) {
-				[valuesArray2[`${j}`], valuesArray2[`${j+1}`]] = [valuesArray2[`${j+1}`], valuesArray2[`${j}`]];
-			}
-			j = j + 2;
+		let values2 = {
+			buy_part2: buy_part2,
+			sell_part2: buy_part2.map(i => i * ((Math.random() * 0.01) + 1.015))
+		};
+
+		for (let i = 0; i < values2.length / 2; i++) {
+			cellText[i].textContent = `${tableValues(values2.buy_part2)[i]}`;
+			cellText[i + 1].textContent = `${tableValues(values2.sell_part2)[i]}`;
 		}
 
-		for (let i = 0; i < valuesArray2.length; i++) {
-			cellText[i].textContent = `${tableValues(valuesArray2)[i]}`;
-		}
-		Array1 == valuesArray2;
+		buy_part1 == buy_part2;
 
-		return valuesArray2;
+		return values2;
 	}
 
-	window.setInterval(dataTable, 2000);
+	function getAllData() {
 
-	/* Choosing currency */
-	let currencyList = document.querySelector('#currency');
-	currencyList.addEventListener('change', currencySelected);
-
-	function currencySelected() {
-		let currencyIndex = currencyList.selectedIndex;
-		let currencyValue;
-
-		function values(name) {
-			let buyValue = document.querySelector(`#${name}_buy`).innerHTML;
-			let sellValue = document.querySelector(`#${name}_sell`).innerHTML;
-			currencyValue = [Number(buyValue), Number(sellValue)];
-
-			return currencyValue;
-		}
-
-
-		switch (currencyIndex) {
-			case 1:
-				currencyValue == values('USD');
-				break;
-			case 2:
-				currencyValue == values('EUR');
-				break;
-			case 3:
-				currencyValue == values('GBP');
-				break;
-			case 4:
-				currencyValue == values('CHF');
-				break;
-		}
-
-		return currencyValue;
 	}
 
-	let currencyBtn = document.querySelector('#currency-btn');
-	currencyBtn.addEventListener('click', chart);
+	/* Taking all buy & sell values from array */
+	let currencyList = document.querySelector('#quantity');
+	currencyList.addEventListener('change', valueSelected);
 
+	function valueSelected() {
+		let intervalNumber = currencyList.value;
+		console.log(intervalNumber);
+		for (let i = 1; i <= intervalNumber; i++) {
+			let allBuy = dataTable().buy_part2;
+			let allSell = dataTable().sell_part2;
+			console.log(allBuy, allSell);
+			// chart();
+			window.setTimeout(dataTable, 2000);
+		}
+	}
+
+	let intervalsBtn = document.querySelector('#intervals-btn');
+	intervalsBtn.addEventListener('click', valueSelected);
+
+	/* Chart displaying */
 	const canvas = document.querySelector('#myCanvas');
 	const ctx = canvas.getContext('2d');
-
 	const cW = canvas.width;
 	const cH = canvas.height;
-	
+
 
 	function chart() {
 		ctx.clearRect(0, 0, cW, cH);
 
-		currencySelected();		
+		valueSelected();
 
-		let buy = currencySelected()[0];
-/* Y axis was scaling 10 times and (0,0) point was moving to bottom-left */
+		let buy = valueSelected()[0];
+		/* Y axis was scaling 10 times and (0,0) point was moving to bottom-left */
 		let Y = cH - 10 * buy;
-console.log(Y);
+		console.log(Y);
 		/*** Drawing lines into the canvas ***/
 		ctx.fillStyle = 'red';
 		ctx.beginPath();
@@ -129,13 +107,7 @@ console.log(Y);
 		for (let i = 1; i <= 10; i++) {
 			ctx.lineTo((cW / 10) * i, Y);
 		}
-
-		let sell = currencySelected()[1];
-		
-    }
-	chart();
-
-		// window.requestAnimationFrame(chart);
+	}
 
 	/* All values will go into the array and then transfer on the chart (canvas) */
 	const chartData = document.querySelector('#chartbox');
